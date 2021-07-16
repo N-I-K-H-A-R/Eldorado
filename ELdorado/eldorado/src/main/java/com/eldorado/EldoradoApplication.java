12bd503b4c484cdb95a49318defb126fc57e6a99
N-I-K-H-A-R <80140165+N-I-K-H-A-R@users.noreply.github.com>
2021-07-16 10:14:29 +0530

    Add files via upload

---
+++ ELdorado/eldorado/src/main/java/com/eldorado/EldoradoApplication.java
@@ -0,0 +1,13 @@
+package com.eldorado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EldoradoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EldoradoApplication.class, args);
+	}
+
+}
